906cdcfb90014c84763b2ab24fb29f98050e5ec8
Piotr <hunter976@gmail.com>
2024-10-12 01:41:24 +0200

    added numbers to the data challenges

--- data/NewbieChallenges.js
+++ data/NewbieChallenges.js
@@ -1,96 +1,112 @@
 export const challenge = [
   {
     name: 'Recipe Main Page',
+    number: 1,
     id: 'recipe',
     pageLink: './Newbie/Recipe-Page',
     css: 'recipe'
   },
   {
     name: 'Social links profile',
+    number: 2,
     id: 'social',
     pageLink: './Newbie/Social-links-profile',
     css: 'social'
   },
   {
     name: 'Blog preview',
+    number: 3,
     id: 'blog',
     pageLink: './Newbie/Blog-preview',
     css: 'blog'
   },
   {
     name: 'FAQ accordion',
+    number: 4,
     id: 'faq',
     pageLink: './Newbie/FAQ-accordion',
     css: 'faq'
   },
   {
     name: 'Workit landing page',
+    number: 5,
     id: 'workit',
     pageLink: './Newbie/Workit-landing-page',
     css: 'workit'
   },
   {
     name: 'Results summary component',
+    number: 6,
     id: 'results',
     pageLink: './Newbie/Result-summary',
     css: 'results'
   },
   {
     name: 'Product Preview Card',
+    number: 7,
     id: 'product-preview',
     pageLink: './Newbie/Product-Preview-Card',
     css: 'product-preview'
   },
   {
     name: 'Skilled elearning page',
+    number: 8,
     id: 'skilled-elearning',
     pageLink: './Newbie/Skilled-Elearning-Page',
     css: 'skilled-elearning'
   },
   {
     name: 'Rating Component',
+    number: 9,
     id: 'rating-component',
     pageLink: './Newbie/rating-component',
     css: 'rating-component'
   },
   {
     name: 'Qr Code Component',
+    number: 10,
     id: 'qr-code',
     pageLink: './Newbie/qr-code-component',
     css: 'qr-code'
   },
   {
     name: 'NFT Preview card',
+    number: 11,
     id: 'nft-preview',
     pageLink: './Newbie/nft-preview-card',
     css: 'nft-preview'
   },
   {
     name: 'Equalizer Landing Page',
+    number: 12,
     id: 'equalizer-landing',
     pageLink: './Newbie/equalizer-landing-page',
     css: 'equalizer-landing'
   },
   {
     name: 'Order Summary Component',
+    number: 13,
     id: 'order-summary',
     pageLink: './Newbie/order-summary-component',
     css: 'order-summary'
   },
   {
     name: 'Stats Preview Card',
+    number: 14,
     id: 'stats-preview',
     pageLink: './Newbie/stats-preview-card',
     css: 'stats-preview'
   },
   {
     name: 'Meet Landing Page',
+    number: 15,
     id: 'meet-landing',
     pageLink: './Newbie/meet-landing-page',
     css: 'meet-landing'
   },
   {
     name: 'Coming Soon',
+    number: '',
     id: '',
     pageLink: '#',
     css: ''

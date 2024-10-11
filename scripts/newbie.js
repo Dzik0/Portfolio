514455506f62c51069cd0faaf2eeb1f34f4662f0
Piotr <hunter976@gmail.com>
2024-10-11 04:17:02 +0200

    created newbie js and moved js there

---
+++ scripts/newbie.js
@@ -0,0 +1,35 @@
+function goPage (page) {
+  if (page === 'back') {
+    window.location.href = '/Portfolio';
+  } else if (page === 'recipe') {
+    window.location.href = './Recipe-Page';
+  } else if (page === 'social') {
+    window.location.href = './Social-links-profile';
+  } else if (page === 'blog') {
+    window.location.href = './Blog-preview';
+  } else if (page === 'faq') {
+    window.location.href = './FAQ-accordion';
+  } else if (page === 'workit') {
+    window.location.href = './Workit-landing-page';
+  } else if (page === 'results') {
+    window.location.href = './Result-summary';
+  } else if (page === 'product-preview-js') {
+    window.location.href = './Product-Preview-Card';
+  } else if (page === 'skilled-elearning') {
+    window.location.href = './Skilled-Elearning-Page';
+  } else if (page === 'rating-component') {
+    window.location.href = './rating-component';
+  } else if (page === 'qr-code') {
+    window.location.href = './qr-code-component';
+  } else if (page === 'nft-preview') {
+    window.location.href = './nft-preview-card';
+  } else if (page === 'equalizer-landing') {
+    window.location.href = './equalizer-landing-page';
+  } else if (page === 'order-summary') {
+    window.location.href = './order-summary-component';
+  } else if (page === 'stats-preview') {
+    window.location.href = './stats-preview-card';
+  } else if (page === 'meet-landing') {
+    window.location.href = './meet-landing-page';
+  } 
+}

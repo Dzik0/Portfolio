13d5f18d6729ae4c2ba00f319e9a7d4f72a07fa3
Piotr <hunter976@gmail.com>
2024-10-11 05:39:30 +0200

    created data with all the newbie challenges

---
+++ data/NewbieChallenges.js
@@ -0,0 +1,98 @@
+export const challenge = [
+  {
+    name: 'Recipe Main Page',
+    id: 'recipe',
+    pageLink: './Newbie/Recipe-Page',
+    css: 'recipe'
+  },
+  {
+    name: 'Social links profile',
+    id: 'social',
+    pageLink: './Newbie/Social-links-profile',
+    css: 'social'
+  },
+  {
+    name: 'Blog preview',
+    id: 'blog',
+    pageLink: './Newbie/Blog-preview',
+    css: 'blog'
+  },
+  {
+    name: 'FAQ accordion',
+    id: 'faq',
+    pageLink: './Newbie/FAQ-accordion',
+    css: 'faq'
+  },
+  {
+    name: 'Workit landing page',
+    id: 'workit',
+    pageLink: './Newbie/workit-landing-page',
+    css: 'workit'
+  },
+  {
+    name: 'Results summary component',
+    id: 'results',
+    pageLink: './Newbie/Result-summary',
+    css: 'results'
+  },
+  {
+    name: 'Product Preview Card',
+    id: 'product-preview',
+    pageLink: './Newbie/Product-Preview-Card',
+    css: 'product-preview'
+  },
+  {
+    name: 'Skilled elearning page',
+    id: 'skilled-elearning',
+    pageLink: './Newbie/Skilled-Elearning-Page',
+    css: 'skilled-elearning'
+  },
+  {
+    name: 'Rating Component',
+    id: 'rating-component',
+    pageLink: './Newbie/rating-component',
+    css: 'rating-component'
+  },
+  {
+    name: 'Qr Code Component',
+    id: 'qr-code',
+    pageLink: './Newbie/qr-code-component',
+    css: 'qr-code'
+  },
+  {
+    name: 'NFT Preview card',
+    id: 'nft-preview',
+    pageLink: './Newbie/nft-preview-card',
+    css: 'nft-preview'
+  },
+  {
+    name: 'Equalizer Landing Page',
+    id: 'equalizer-landing',
+    pageLink: './Newbie/equalizer-landing-page',
+    css: 'equalizer-landing'
+  },
+  {
+    name: 'Order Summary Component',
+    id: 'order-summary',
+    pageLink: './Newbie/order-summary-component',
+    css: 'order-summary'
+  },
+  {
+    name: 'Stats Preview Card',
+    id: 'stats-preview',
+    pageLink: './Newbie/stats-preview-card',
+    css: 'stats-preview'
+  },
+  {
+    name: 'Meet Landing Page',
+    id: 'meet-landing',
+    pageLink: './Newbie/meet-landing-page',
+    css: 'meet-landing'
+  },
+  {
+    name: 'Coming Soon',
+    id: '',
+    pageLink: '#',
+    css: ''
+  },
+];

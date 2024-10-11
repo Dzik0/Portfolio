ba88aa2ab2cb2b7ab946b8ed8b4b9432b6cd0abd
Piotr <hunter976@gmail.com>
2024-10-11 05:39:17 +0200

    created showing js btns +  made them interactive

--- scripts/newbie.js
+++ scripts/newbie.js
@@ -1,35 +1,34 @@
-function goPage (page) {
-  if (page === 'back') {
-    window.location.href = '/Portfolio';
-  } else if (page === 'recipe') {
-    window.location.href = './Recipe-Page';
-  } else if (page === 'social') {
-    window.location.href = './Social-links-profile';
-  } else if (page === 'blog') {
-    window.location.href = './Blog-preview';
-  } else if (page === 'faq') {
-    window.location.href = './FAQ-accordion';
-  } else if (page === 'workit') {
-    window.location.href = './Workit-landing-page';
-  } else if (page === 'results') {
-    window.location.href = './Result-summary';
-  } else if (page === 'product-preview-js') {
-    window.location.href = './Product-Preview-Card';
-  } else if (page === 'skilled-elearning') {
-    window.location.href = './Skilled-Elearning-Page';
-  } else if (page === 'rating-component') {
-    window.location.href = './rating-component';
-  } else if (page === 'qr-code') {
-    window.location.href = './qr-code-component';
-  } else if (page === 'nft-preview') {
-    window.location.href = './nft-preview-card';
-  } else if (page === 'equalizer-landing') {
-    window.location.href = './equalizer-landing-page';
-  } else if (page === 'order-summary') {
-    window.location.href = './order-summary-component';
-  } else if (page === 'stats-preview') {
-    window.location.href = './stats-preview-card';
-  } else if (page === 'meet-landing') {
-    window.location.href = './meet-landing-page';
-  } 
+//IMPORTED
+import { challenge } from "../data/NewbieChallenges.js";
+
+//FUNCTION THAT TAKES YOU TO THE PROPER CHALLENGE
+function goPage (challengeId) {
+  let matchingItem = challenge.find(item => item.id === challengeId);
+  
+  window.location.href = `${matchingItem.pageLink}`
 }
+
+// Stored html
+let displayedHTML = '';
+
+
+//Looping through all challenges data
+challenge.forEach((challenge) => {
+  displayedHTML += `
+    <button class="${challenge.css} js-btn id-${challenge.id}" data-page-id="${challenge.css}">${challenge.name}</button>
+  `
+});
+
+//Displaying on page
+document.querySelector('.btn-container-js')
+  .innerHTML = displayedHTML;
+
+//Making btns interactive
+document.querySelectorAll('.js-btn').forEach((button) => {
+  button.addEventListener('click', () => {
+    const buttonId = button.dataset.pageId;
+    const challengeId = buttonId;
+    
+    goPage(challengeId);
+  });
+});

d54602a5f14153fce68f4d341c82624c8fe4a673
Piotr <Hunter976@gmail.com>
2024-12-29 00:37:34 +0100

    Added new challenge + updated data

--- data/JuniorChallenges.js
+++ data/JuniorChallenges.js
@@ -1,9 +1,16 @@
 export const challenge = [
   {
-    name: 'Bonto grid',
+    name: "Bonto grid",
     number: 1,
-    id: 'bento',
-    pageLink: './Junior/bento-grid',
-    css: 'bento'
-  }
-]
+    id: "bento",
+    pageLink: "./Junior/bento-grid",
+    css: "bento",
+  },
+  {
+    name: "Book Club landing page",
+    number: 2,
+    id: "bookclub",
+    pageLink: "./Junior/tech-book-club-landing-page",
+    css: "bookclub",
+  },
+];
